a5ece32edf344d43c20fe1b84b27660daf360bcb
my-staffmanager-software <34810211+my-staffmanager-software@users.noreply.github.com>
2020-06-26 10:18:15 +0100

    Create Q1.java

---
+++ Q1.java
@@ -0,0 +1,40 @@
+package upperlink;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q1 {
+
+    //Time complexity = O(n0, Space O(n)
+    private static String duplicate(String input){
+
+        String[] content = input.split("");
+        HashMap<String,Integer> m = new HashMap<>();
+
+        for(String s : content){
+
+            if( m.containsKey(s) ){
+                int val = m.get(s);
+                m.put(s, val+1);
+            }else
+                m.put(s,1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry<String,Integer> k : m.entrySet()) {
+
+            if (k.getValue() > 1) {
+                sb.append(k.getKey() + " == " + k.getValue());
+                sb.append("\n");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(duplicate("JAVA"));
+        System.out.println(duplicate("UPPERLINKERI"));
+    }
+
+}
